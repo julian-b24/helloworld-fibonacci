40f68eee93646fcb616d926b43427c79a1aaad6c
julian-b24 <julian.camilob24@gmail.com>
2023-02-17 17:27:44 -0500

    Initial Commit

---
+++ client/src/main/java/Client.java
@@ -0,0 +1,22 @@
+public class Client
+{
+    public static void main(String[] args)
+    {
+        java.util.List<String> extraArgs = new java.util.ArrayList<>();
+
+        try(com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize(args,"config.client",extraArgs))
+        {
+            //com.zeroc.Ice.ObjectPrx base = communicator.stringToProxy("SimplePrinter:default -p 10000");
+            Demo.PrinterPrx twoway = Demo.PrinterPrx.checkedCast(
+                communicator.propertyToProxy("Printer.Proxy")).ice_twoway().ice_secure(false);
+            //Demo.PrinterPrx printer = Demo.PrinterPrx.checkedCast(base);
+            Demo.PrinterPrx printer = twoway.ice_oneway();
+
+            if(printer == null)
+            {
+                throw new Error("Invalid proxy");
+            }
+            printer.printString("Hello World!");
+        }
+    }
+}
