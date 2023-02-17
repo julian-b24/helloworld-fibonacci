bf091c2af0d8b9b7e028ac8f29133c9bc20b214e
julian-b24 <julian.camilob24@gmail.com>
2023-02-17 18:43:42 -0500

    Fibonacci implementation added

---
+++ server/src/main/java/utils/FibonacciImpl.java
@@ -0,0 +1,21 @@
+package utils;
+
+import java.util.HashMap;
+
+public class FibonacciImpl implements FibonacciService{
+
+    private static HashMap<Integer, Integer> memory = new HashMap<>();
+
+    @Override
+    public int calculateFibonacci(int number) {
+        if (number <= 1) {
+            return number;
+        } else if (memory.containsKey(number)) {
+            return memory.get(number);
+        } else {
+            int result = calculateFibonacci(number - 1) + calculateFibonacci(number - 2);
+            memory.put(number, result);
+            return result;
+        }
+    }
+}
---
+++ server/src/main/java/utils/FibonacciService.java
@@ -0,0 +1,5 @@
+package utils;
+
+public interface FibonacciService {
+    int calculateFibonacci(int number);
+}
