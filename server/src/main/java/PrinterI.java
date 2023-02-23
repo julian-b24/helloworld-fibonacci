e455f5210dd03877f214bda2d27aeb5ea84ef94b
julian-b24 <julian.camilob24@gmail.com>
2023-02-23 18:53:56 -0500

    Fibonacci server response added and only numbers validation

--- server/src/main/java/PrinterI.java
+++ server/src/main/java/PrinterI.java
@@ -1,9 +1,32 @@
 import com.zeroc.Ice.Current;
+import utils.FibonacciImpl;
+import utils.FibonacciService;
 
 public class PrinterI implements Deploy.FibonacciPrinter
 {
     @Override
     public int printFibonacci(String hostname, String input, Current current) {
-        return 0;
+        showMessageCmd(hostname, input);
+        int output = 0;
+        if(validateInputIsNumber(input)){
+            output = calculateFibonacci(input);
+        }
+        return output;
+    }
+
+    private int calculateFibonacci(String input) {
+        int inputNumber = new Integer(input);
+        FibonacciService fibonacciService = new FibonacciImpl();
+        int result = fibonacciService.calculateFibonacci(inputNumber);
+        return result;
+    }
+
+    private void showMessageCmd(String hostname, String input){
+        String message = hostname + ": " + input;
+        System.out.println(message);
+    }
+
+    private boolean validateInputIsNumber(String input){
+        return input.matches("\\d+");
     }
 }
