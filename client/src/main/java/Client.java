24a4c71fe5bb1392e12dae589f62165c9d61d667
julian-b24 <julian.camilob24@gmail.com>
2023-02-23 19:02:50 -0500

    Client validation and whoami added

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -8,6 +8,7 @@ import java.net.UnknownHostException;
 
 public class Client {
 
+    private final static BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
     private final static String EXIT_STRING = "exit";
 
 
@@ -36,15 +37,19 @@ public class Client {
         String hostname = getHostName();
         String input = "";
         do {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            input = reader.readLine();
-            System.out.println(printer.printFibonacci(hostname, input));
-
+            System.out.println("Input:");
+            input = READER.readLine();
+            if(!input.equals(EXIT_STRING)){
+                System.out.println(printer.printFibonacci(hostname, input));
+            }
         } while (!input.equals(EXIT_STRING));
     }
 
-    private static String getHostName() throws UnknownHostException {
-        return InetAddress.getLocalHost().getHostName();
+    private static String getHostName() throws IOException {
+        System.out.println("Who are you? :");
+        String whoami = READER.readLine();
+        String hostname = InetAddress.getLocalHost().getHostName();
+        return whoami + "@" + hostname;
     }
 
 }
